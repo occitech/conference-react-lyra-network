0aecdd4f8198072eca3ec19337df4e564a7278aa
Julien Pradet <julien@occitech.fr>
2016-11-02 23:10:59 +0100

    Improve presentation style and add code examples

--- presentation/3-migration.js
+++ presentation/3-migration.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Slide, Heading, Text, Appear, CodePane, List, ListItem} from 'spectacle'
+import {Slide, Heading, Text, Appear, CodePane, List, ListItem, Image} from 'spectacle'
 
 export default [
   <Slide key='3-0'>
@@ -13,18 +13,16 @@ export default [
     </Text>
   </Slide>,
   <Slide key='3-1-1' notes={`Exemple d'utilisation dans un framework`}>
-    <Text textColor='tertiary'>
-      Angular - ngReact
+    <Text textAlign='left' textColor='tertiary'>
+      ngReact
     </Text>
-  </Slide>,
-  <Slide key='3-1-2' notes={`Exemple d'utilisation dans un framework`}>
-    <Text textColor='tertiary'>
-      Backbone - React
+    <Text textAlign='left' textColor='tertiary'>
+      Backbone.React.Component
     </Text>
   </Slide>,
   <Slide key='3-1-3' notes={`Exemple d'utilisation au milieu de nul part. Par contre, gérer votre état de manière globale sera important.`}>
-    <Text textColor='tertiary'>
-      {`$('.selector').each(() => ReactDOM.render(<Element />, this))`}
+    <Text textColor='tertiary' textAlign='left'>
+      ReactDOM.render(<br />&nbsp;&nbsp;{'<Element />'},<br />&nbsp;&nbsp;document.querySelector('.selector')<br />)
     </Text>
   </Slide>,
   <Slide key='3-1-4' notes={`Si des bouts sont liés`}>
@@ -51,14 +49,14 @@ export default [
   </Slide>,
   <Slide key='3-2-0' notes={`Exemple appli cartographie => Gain notamment sur la mise à jour des options disponibles. Y compris par rapport au niveau de zoom`}>
     <Text textColor='tertiary'>Migration de l'UI d'une cartographie</Text>
-    <Text textColor='tertiary'>Schema</Text>
+    <Text textColor='tertiary'><br />Schema</Text>
   </Slide>,
   <Slide key='3-2-1' notes={`Exemple appli cartographie`}>
     <Text textColor='tertiary'>Migration progressive</Text>
     <List textColor='tertiary'>
       <Appear><ListItem>Tooltips</ListItem></Appear>
       <Appear><ListItem>Données à afficher</ListItem></Appear>
-      <Appear><ListItem>Choix de la carte</ListItem></Appear>
+      <Appear><ListItem>Choix de l'imagerie</ListItem></Appear>
     </List>
   </Slide>,
   <Slide key='3-2-2' notes={`Pas besoin donc on ne va pas trop loin`}>
@@ -68,33 +66,38 @@ export default [
     <Heading textColor='secondary' fit caps>Quelques bonnes pratiques</Heading>
   </Slide>,
   <Slide key='3-3-1' notes={`Quelques bonnes pratiques`}>
-    <Text textColor='tertiary' textSize='2em'>Préferrez trop découper que pas assez</Text>
+    <Text textColor='tertiary' textSize='2em'>Préferez trop découper que pas assez</Text>
     <Appear>
-      <Text textColor='tertiary'><br />pas de if, pas de boucle, pas de variables<br />CodeExample</Text>
+      <Text textColor='quartenary'><br />pas de if, pas de boucle, pas de variables</Text>
     </Appear>
   </Slide>,
   <Slide key='3-3-3' notes={`Attention, ca ne veut pas dire stateful vs stateless.<br />En plus ca permet de ne pas être trop couplé à React => Controllers`}>
     <Text textColor='tertiary' textSize='2em'>Séparer l'UI du métier</Text>
     <Appear>
-      <Text textColor='tertiary'><br />Smart vs Dumb<br />Container vs Component</Text>
+      <Text textColor='quartenary'><br />Component vs Container</Text>
     </Appear>
   </Slide>,
   <Slide key='3-3-2' notes={`Préparer RN.<br /> En plus ca permettrait de changer de lib de vue. Chercher virtual DOM`}>
     <Text textColor='tertiary' textSize='2em'>Balises HTML le plus tard possible</Text>
     <Appear>
-      <Text textColor='tertiary'><br />Force à se poser des questions sur la réutilisabilité de l'UI</Text>
+      <Text textColor='quartenary'><br />Force à se poser des questions sur la réutilisabilité de l'UI</Text>
     </Appear>
     <Appear>
-      <Text textColor='tertiary'>Permet de préparer le multi-plateforme</Text>
+      <Text textColor='quartenary'>Permet de préparer le multi-plateforme</Text>
     </Appear>
   </Slide>,
   <Slide key='3-3-3' notes={`Développez vos composants de manière isolée`}>
     <Text textColor='tertiary' textSize='2em'>Un composant doit fonctionner seul</Text>
     <Appear>
-      <Text textColor='tertiary'><br />React-Storybook</Text>
+      <Text textColor='quartenary'><br />React-Storybook</Text>
     </Appear>
     <Appear>
-      <Text textColor='tertiary'>Screenshot</Text>
+      <Image
+        src='https://github.com/kadirahq/react-storybook/raw/master/docs/demo.gif'
+        display='block'
+        width='80%'
+        margin='1em auto 0'
+      />
     </Appear>
   </Slide>
 ]
---
+++ presentation/4-front-commerce.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {Slide, Heading, Text, Appear, CodePane, List, ListItem, Image} from 'spectacle'
+
+export default [
+  <Slide key='4-0'>
+    <Heading textColor='secondary' fit caps>
+      Front-Commerce
+    </Heading>
+    <Appear>
+      <Heading textColor='tertiary' caps>
+        Demo
+      </Heading>
+    </Appear>
+  </Slide>,
+  <Slide key='4-1'>
+    <Text textColor='tertiary'>Pourquoi React ?</Text>
+  </Slide>,
+  <Slide key='4-2'>
+    <Text textColor='tertiary'>Pourquoi Falcor ?</Text>
+  </Slide>,
+  <Slide key='4-2'>
+    <Text textColor='tertiary'>Comment mutualiser le code ?</Text>
+  </Slide>,
+  <Slide key='4-2'>
+    <Text textColor='tertiary'>Comment gérer le multi-thème ?</Text>
+  </Slide>,
+]
